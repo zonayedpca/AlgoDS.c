6bd98a131170704c5abaf2760015053fc830ab9b
Zonayed Ahmed <zonayedpca@yahoo.com>
2019-09-25 17:02:05 +0530

    calloc done

---
+++ dynamicmem/calloc.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define ARRAY_SIZE 10
+
+int main() {
+  int *array = calloc(sizeof(int), ARRAY_SIZE);
+  if(array) {
+    for(int i = 0; i < ARRAY_SIZE; i++) {
+      printf("Enter the element: ");
+      scanf("%d", (array + i));
+    }
+    printf("Priting all the elements...");
+    for(int i = 0; i < ARRAY_SIZE; i++) {
+      printf("Element is: %d \n", *(array + i));
+    }
+  } else {
+    printf("Failed to allocate enough memory!\n");
+  }
+  return 0;
+}
